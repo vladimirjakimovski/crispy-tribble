43512305a9ec6807b26a8cd0bc16e226d7e6d5ed
Vladimir Jakimovski <vladimirjakimovski@gmail.com>
2023-03-14 00:23:53 +0100

    single blog post url

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function viewSinglePost(Post $post)
+    {
+        return view('single-post', ['post' => $post]);
+    }
+
+    public function storeNewPost(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        $newPost = Post::create($incomingFields);
+
+        return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created.');
+    }
+
+    public function showCreateForm()
+    {
+        return view('create-post');
+    }
+}
