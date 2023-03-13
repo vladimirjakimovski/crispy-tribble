6f68371f4ef7802cbdf62cb74f2b939867fc9c96
Vladimir Jakimovski <vladimirjakimovski@gmail.com>
2023-03-14 00:37:05 +0100

    Markdown html

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,6 +11,7 @@ class PostController extends Controller
 
     public function viewSinglePost(Post $post)
     {
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><br>');
         return view('single-post', ['post' => $post]);
     }
 
