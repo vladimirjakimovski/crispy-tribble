84bc1eaadfc554bc587f7102408ffb318d715233
Vladimir Jakimovski <vladimirjakimovski@gmail.com>
2023-03-16 23:21:50 +0100

    user profile

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLog
 Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
 Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+
+// Profile related routes
+Route::get('/profile/{user:username}', [UserController::class, 'profile']);
